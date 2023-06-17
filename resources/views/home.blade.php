5a6034325c95b9316b17e8c0c37f27bcd210ad4c
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-06-17 13:02:31 -0300

    Update layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,35 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Myshop - Ecommerce</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-hN4K0qMqojeTq7+RZK0lW1tzd3MhLNPQa5OIGLjg2x5znoPdXh7iR6mG5WrDk7A8lTQH9ZVdj/0c9ka3J4y+2Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+@extends("layout")
+@section("conteudo")
 
-</head>
-<body>
-    <nav class="navbar navbar-light navbar-expand-md bg-light pl-5 pr-5 mb-5">
-        <a href="#" class="navbar-brand">MyShop</a>
-        <div class="collapse navbar-collapse mr-auto">
-            <div class="navbar-nav">
-                <a class="nav-link" href="{{route('home')}}">HOME</a>
-                <a class="nav-link" href="{{route('categoria')}}">Categorias</a>
-                <a class="nav-link" href="{{route('cadastrar')}}">Cadastrar</a>
-            </div>
-        </div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-          </svg>
-    </nav>
+         @if(isset($lista))
+             @foreach ( $lista as $prod)
 
-    <div class="container">
-        <div class="row">
-            @if(isset($lista))
-                @foreach ( $lista as $prod)
-
-                    <div class="col-3 mb-3">
+                 <div class="col-3 mb-3">
                         <div Class="card">
                             <img src="{{asset($prod->foto)}}" class="card-img-top"/>
                             <div class="card-body">
@@ -41,7 +16,4 @@
                     </div>
                 @endforeach
             @endif
-        </div>
-
-</body>
-</html>
+@endsection
