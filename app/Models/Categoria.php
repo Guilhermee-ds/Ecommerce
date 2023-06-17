93139c16816d1ed5d168b5857a39ebc6a8ac7b01
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-06-17 14:41:05 -0300

    update bladed

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -5,6 +5,5 @@ namespace App\Models;
 
 class Categoria extends RModel
 {
-    protected $table = 'categorias';
-    protected $fillable = ['categoria'];
+
 }
--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -5,7 +5,5 @@ namespace App\Models;
 
 class Produto extends RModel
 {
-    protected $table = 'produtos';
-    protected $fillable = ['nome', 'foto', 'descricao', 'categoria_id', 'valor'];
 
 }
