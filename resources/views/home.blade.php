518867b90fe7965b7717a6c984cdcfc8261389af
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-06-18 17:07:38 -0300

    update bug

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,20 +23,25 @@
             <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
           </svg>
     </nav>
-@section()
-         @if(isset($lista))
-             @foreach ( $lista as $prod)
 
-                 <div class="col-3 mb-3">
+    <div class="container">
+        <div class="row">
+            @if(isset($lista))
+                @foreach ( $lista as $prod)
+
+                    <div class="col-3 mb-3">
                         <div Class="card">
                             <img src="{{asset($prod->foto)}}" class="card-img-top"/>
                             <div class="card-body">
                                 <h6 class="card-title">{{ $prod->nome }}</h6>
                                 <h4 class="card-title">R$:{{ $prod->valor}}</h4>
-                                <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                                <a href="#" class="btn btn-sm btn-secondary">Adicionar Ao carrinho</a>
                             </div>
                         </div>
                     </div>
                 @endforeach
             @endif
-@endsection
+        </div>
+
+</body>
+</html>
